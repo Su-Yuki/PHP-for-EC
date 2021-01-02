a95225010fcef482a1ac938dc1100d2dd934de53
鈴木悠祐 <>
2021-01-02 23:21:56 +0900

    suck

--- staff_add_check.php
+++ staff_add_check.php
@@ -45,15 +45,3 @@ if($staff_name == '' || $staff_pass == '' || $staff_pass != $staff_pass2){
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
