2962ae9f75aac1482d1d50c0c79bd5b4fcff8591
鈴木悠祐 <>
2021-01-02 23:29:07 +0900

    no message

--- staff_list.php
+++ staff_list.php
@@ -4,20 +4,25 @@ require_once('function.php');
 
 // DBへのデータ保存
 try{
-    $stmt = $dbh->prepare('SELECT name FROM mst_staff WHERE 1 ');
+    $stmt = $dbh->prepare('SELECT code, name FROM mst_staff WHERE 1 ');
     $stmt->execute();//?を変数に置き換えてSQLを実行
 
     print "スタッフ一覧<br /><br />";
 
+    print "<form method='post' action='staff_edit.php' ></form>";
     while(true){
         $rec = $stmt->fetch(PDO::FETCH_ASSOC);
         if($rec == false){
         break;
         }
+        print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
         print $rec["name"];
         print"<br />";
         
     }
+
+    print '<input type="submit">';
+    print '</form>';
 } catch(Exception $e) {
     print "ただいま障害により大変ご迷惑をおかけしております。";
     exit;
