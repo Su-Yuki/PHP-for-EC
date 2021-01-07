617e6c8f6eaca5be120a5b1dd1b867323ddc7156
Su-Yuki <76637981 +Su-Yuki@users.noreply.github.com>
2021-01-08 04:48:20 +0900

    s-edit-form

---
+++ staff_edit.php
@@ -0,0 +1,51 @@
+<?php
+require_once('dbconnect.php');
+require_once('function.php');
+// var_dump($_POST['staffcode']);
+
+try{
+    $staff_code = $_POST['staffcode'];
+
+    $stmt = $dbh->prepare('SELECT name FROM mst_staff WHERE code = ? ');
+    $data[] = $staff_code;// var_dump($data);
+    $stmt->execute($data);
+
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);// var_dump($rec);
+    $staff_name = $rec['name'];// var_dump($rec['name']);
+
+
+} catch(Exception $e) {
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>スタッフ修正</p>
+    <br>
+    <?php print $staff_code;?>
+    <br>
+    <br>
+    <form action="staff_edit_check.php" method="POST">
+        <input type="hidden" name="code" value="<?php echo $staff_code; ?>">
+        <p>スタッフ名</p>
+        <input type="text" name="name" style="width:200px" value="<?php echo $staff_name?>">
+        <br>
+        <p>パスワードを入力してください</p>
+        <input type="password" name="pass" style="width: 100px">
+        <br>
+        <p>パスワードをもう一度入力してください</p>
+        <input type="password" name="pass2" style="width: 100px">
+        <br>
+        <input type="button" onclick="history.back()" value="戻る">
+        <input type="submit" value="OK">
+    </form>
+</body>
+</html>
--- staff_list.php
+++ staff_list.php
@@ -9,7 +9,7 @@ try{
 
     print "スタッフ一覧<br /><br />";
 
-    print "<form method='post' action='staff_edit.php' ></form>";
+    print "<form method='post' action='staff_edit.php' >";
     while(true){
         $rec = $stmt->fetch(PDO::FETCH_ASSOC);
         if($rec == false){
@@ -21,7 +21,7 @@ try{
         
     }
 
-    print '<input type="submit">';
+    print '<input type="submit" value="修正">';
     print '</form>';
 } catch(Exception $e) {
     print "ただいま障害により大変ご迷惑をおかけしております。";
